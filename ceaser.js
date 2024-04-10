7ac0bae45aad8b0ce61811e4934a274aee6e9423
SoosyPoo <162006443+SoosyPoo@users.noreply.github.com>
2024-04-10 14:08:08 -0700

    Create ceaser.js

---
+++ ceaser.js
@@ -0,0 +1,13 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+//const variable = "abcdefghijklmnopqrstuvwxyz";
+//const firstLetter = friend[0];
+//const index = alphabet.indexOf(firstLetter.toLowerCase());
+//const newIndex = index + shiftValue;
+//const encryptedFirstLetter = alphabet[newIndex];
+//const alphabetLength = alphabet.length;
+//encryptedFirstLetter = alphabet[newIndex];
+
+//const encryptedMessage = "EUXWXV";
+//const teaserMessage = encryptedMessage.slice(0, 3);
+
